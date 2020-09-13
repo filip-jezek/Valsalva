b8c01bb98b332d957acd66b5d7936d79ec8e67ce
Filip Ježek <filip.jezek@lf1.cuni.cz>
2020-09-13 12:51:50 +0200

    Fixing Q_mitral valve optim target and seting to best params vals to prepare next run.

--- Identification/optimizeBaselineTriSegLumens/cost_function.py
+++ Identification/optimizeBaselineTriSegLumens/cost_function.py
@@ -77,7 +77,7 @@ def getObjectives(vars_set):
     # mitral valve flow ratio spontaneous:atrial contraction is about 2:1 
     vla_1st_Peak_i = fun_lib.findInterval(39.8, 40, time)
     vla_2nd_Peak_i = fun_lib.findInterval(39.2, 39.4, time)
-    vla_peak_frac = numpy.max(vars_set['V_la'][vla_1st_Peak_i])/numpy.max(vars_set['V_la'][vla_2nd_Peak_i])
+    vla_peak_frac = numpy.max(vars_set['q_mv'][vla_1st_Peak_i])/numpy.max(vars_set['q_mv'][vla_2nd_Peak_i])
     vla_peak_target = 2
 
     # Van Bortel 2012 siggest using 80 % of carotid to femoral distance
@@ -96,7 +96,7 @@ def getObjectives(vars_set):
             ('ESV', numpy.min(vars_set['V_LV'][interval]), 60*ml2SI, None, 1),
             ('ESV_la', numpy.min(vars_set['V_la'][interval]), 41*ml2SI, None, .1),
             ('EDV_la', numpy.max(vars_set['V_la'][interval]), 87*ml2SI, None, .1),
-            ('Q_MV_f', vla_peak_frac, vla_peak_target, None, .1),
+            ('Q_MV_f', vla_peak_frac, vla_peak_target, None, 1),
             
 # set by assumption and loop closed
 #            ('HR', numpy.mean(vars_set['HR'][interval]), HR_target, None, 1), 
