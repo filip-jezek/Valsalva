a0b31fb332ee3e0939387af4b048c589802d121f
Filip Ježek <filip.jezek@lf1.cuni.cz>
2021-06-17 00:41:25 +0200

    UPdate fig export for summary figure

--- Scripts/Figures/Figs_R_SUM.m
+++ Scripts/Figures/Figs_R_SUM.m
@@ -1,30 +1,45 @@
-%% plots the baseline results
-datafile = '../../Results/CardiovascularSystem.mat'
-% import the dymload util
-addpath('c:\Program Files\Dymola 2021\Mfiles\dymtools\')
-dl = dymload(datafile)
-
+%% plots all the figures for figure summary
 %% color defiinitons
 color_b = [28, 108, 200]/255;
 color_r = [238, 46, 47]/255;
 color_g = [0, 140, 72]/255;
 color_m = [226, 113, 199]/255;
 color_lb = [182, 226, 255]/255;
-
-%%
 mmHg2SI = 133.322;
 ml2SI = 1e-6;
-%%
-time = dymget(dl, 'Time');
-t_interval = [28.45, 29.45]; % interval in seconds
-td = t_interval(2) - t_interval(1)
-% i_int = [find(t >= t_interval(1), 1), find(t >= t_interval(2), 1)-1];
-i_int = (time >= t_interval(1) & time <= t_interval(2));
+bpm2SI = 1/60;
+%% plots the baseline results
+datafile = '../../Results/CardiovascularSystem.mat'
+% import the dymload util
+addpath('c:\Program Files\Dymola 2021\Mfiles\dymtools\')
+dl = dymload(datafile)
 
-t = time - t_interval(1);
+time = dymget(dl, 'Time');
+t_interval_bl = [28.45, 29.45]; % interval in seconds
+t_interval_bl2 = [28.45, 29.95]; % interval in seconds
+td = t_interval_bl(2) - t_interval_bl(1)
+% i_int = [find(t >= t_interval(1), 1), find(t >= t_interval(2), 1)-1];
+i_int_bl = (time >= t_interval_bl(1) & time <= t_interval_bl(2));
+i_int_bl2= (time >= t_interval_bl2(1) & time <= t_interval_bl2(2));
+
+t_bl = time - t_interval_bl(1);
+t_bl2 = time - t_interval_bl2(1);
 A_p1 = dymget(dl, 'SystemicComponent.ascending_aorta_A.p1')/mmHg2SI;
 A_p2 = dymget(dl, 'SystemicComponent.internal_carotid_R8_B.p_C')/mmHg2SI;
 A_p3 = dymget(dl, 'brachial_pressure')/mmHg2SI;
+
+B1_vlv = dymget(dl, 'V_LV')/ml2SI;
+B1_vla = dymget(dl, 'V_la')/ml2SI;
+
+B1_pv = dymget(dl, 'heartComponent.pv.pressure')/mmHg2SI;
+B1_psa = A_p1;
+B1_plv = dymget(dl, 'heartComponent.mitralValve.q_out.pressure')/mmHg2SI;
+B1_pla = dymget(dl, 'heartComponent.mitralValve.q_in.pressure')/mmHg2SI;
+
+B1_vrv = dymget(dl, 'heartComponent.ventricles.V_RV')/ml2SI;
+B1_prv = dymget(dl, 'heartComponent.ventricles.P_RV')/mmHg2SI;
+B1_vra = dymget(dl, 'heartComponent.ra.volume')/ml2SI;
+
 
 psa = dymget(dl, 'heartComponent.sa.pressure')/mmHg2SI;
 plv = dymget(dl, 'heartComponent.mitralValve.q_out.pressure')/mmHg2SI;
@@ -40,23 +55,22 @@ pra = dymget(dl, 'heartComponent.tricuspidValve.q_in.pressure')/mmHg2SI;
 vrv = dymget(dl, 'heartComponent.ventricles.V_RV')/ml2SI;
 vra = dymget(dl, 'heartComponent.ra.volume')/ml2SI;
 
+pa_tibial = dymget(dl, 'SystemicComponent.posterior_tibial_T4_L214.p_in')/mmHg2SI;
+pv_tibial = dymget(dl, 'SystemicComponent.posterior_tibial_T4_L214.p_out')/mmHg2SI;
 qmv = dymget(dl, 'q_mv')/ml2SI/1000*60;
-%% A
+%% SIZE
 fig1 = figure(1);clf;
-set(gcf, 'DefaultAxesFontSize', 10, 'defaultLineLineWidth',1.0);
-tw = 27/3;
-th = 27/3*400/513;
+set(gcf, 'DefaultAxesFontSize', 6, 'defaultLineLineWidth',1.0);
+tw = 5.8;
+th = 4.5;
 set(gcf, 'Units', 'Centimeters', 'Position', [0, 0, tw, th])
-% s_a1 = subplot(4, 2, 1);
-% a_a1 = axes()
-% a_a1 = gca()
-
 hold on;
+%% A1
 % title('A: Left ventricular pressures and volumes', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'top');
 % title('A: Left ventricular pressures and volumes');
-plot(t(i_int), A_p1(i_int), 'LineWidth', 1.5, 'Color', color_b);
-plot(t(i_int), A_p2(i_int), 'LineWidth', 1.5, 'Color', color_r);
-plot(t(i_int), A_p3(i_int), 'LineWidth', 1.5, 'Color', color_g);
+plot(t_bl(i_int_bl), A_p1(i_int_bl), 'LineWidth', 1, 'Color', color_b);
+plot(t_bl(i_int_bl), A_p2(i_int_bl), 'LineWidth', 1, 'Color', color_r);
+plot(t_bl(i_int_bl), A_p3(i_int_bl), 'LineWidth', 1, 'Color', color_g);
 % plot(t(i_int), pb(i_int));
 % set(gca,'xtick',[])
 leg = legend('Asc Aor', 'Int carotid', 'Brach art', 'Location', 'NorthEast')
@@ -68,121 +82,270 @@ xlabel('t (s)')
 % s_a1.Clipping = 'off';
 xlim([0 td])
 
-exportgraphics(gcf,'fig_R_sum_A.png','Resolution',300)
-exportgraphics(gcf,'fig_R_SUM_A.pdf', 'ContentType','vector')
+% exportgraphics(gcf,'fig_R_sum_A.png','Resolution',300)
+exportgraphics(gcf,'fig_R_SUM_A1.pdf', 'ContentType','vector')
 
-%%    
-% volumes
-s_a2 = subplot(4, 2, 3);hold on;
-plot(t(i_int), vlv(i_int), 'b');
-plot(t(i_int), vla(i_int), 'r');
-set(gca,'xtickMode', 'auto')
-ylim([0, 150]);
+%% A2
+figure(1);clf; hold on;
+set(gcf, 'DefaultAxesFontSize', 6, 'defaultLineLineWidth',1.0);
+tw = 6.2;
+th = 4.5;
+set(gcf, 'Units', 'Centimeters', 'Position', [0, 0, tw, th])
+
+plot(t_bl(i_int_bl), B1_pv(i_int_bl), 'LineWidth', 1, 'Color', color_m);
+plot(t_bl(i_int_bl), B1_pla(i_int_bl), 'LineWidth', 1, 'Color', color_g);
+plot(t_bl(i_int_bl), B1_plv(i_int_bl), 'LineWidth', 1, 'Color', color_r);
+plot(t_bl(i_int_bl), B1_psa(i_int_bl), 'LineWidth', 1, 'Color', color_b);
+ylim([0, 120]);
+ylabel('Pressure (mmHg)')
+xlabel('t (s)')
+yyaxis right;cla;
+% set(gca,'YColor','k');
+plot(t_bl(i_int_bl), B1_vla(i_int_bl), ':', 'LineWidth', 1, 'Color', color_g);
+plot(t_bl(i_int_bl), B1_vlv(i_int_bl), ':', 'LineWidth', 1, 'Color', color_r);
+g = gca; g.YAxis(2).Color = [0,0,0];
+ylim([0, 160]);
+ylabel('Volume (ml)');
+
+leg = legend('P PV', 'P LA', 'P LV','P Asc Aor', 'V LA*', 'V LV*', 'Location', 'East');
+leg.ItemTokenSize = [10, 2];
+% legend('boxoff')
 xlim([0 td])
-xlabel('t (s)');
-ylabel('Volume (ml)')
-% pos = get(s1, 'Position');
-% set(s1, 'Position', [0.05, 0.5, 0.9,0.4])
-% set(s2, 'Position', [0.05, 0.05, 0.9,0.4])
+exportgraphics(gcf,'fig_R_SUM_A2.pdf', 'ContentType','vector')
 
-% B
-s_b1 = subplot(4, 2, 2);cla;
+%% A3
+fig1 = figure(1);clf;
+set(gcf, 'DefaultAxesFontSize', 6, 'defaultLineLineWidth',1.0);
+tw = 5.8;
+th = 4.5;
+set(gcf, 'Units', 'Centimeters', 'Position', [0, 0, tw, th])
 hold on;
-title('B: Right ventricular pressures and volumes');
-plot(t(i_int), prv(i_int), 'b', 'LineWidth', 1);
-plot(t(i_int), ppa(i_int), 'r', 'LineWidth', 1);
-plot(t(i_int), pra(i_int), 'm', 'LineWidth', 1);
-% set(gca,'xtick',[], 'ytick', [])
-set(gca,'xtick',[])
-leg = legend('P RV', 'P Pulm Ar', 'P RA')
-leg.ItemTokenSize = [10, 2];
-% ylim([0, 120])
-xlim([0 td])
+
 % volumes
-s_b2 = subplot(4, 2, 4);hold on;
-plot(t(i_int), vrv(i_int), 'b', 'LineWidth', 1);
-plot(t(i_int), vra(i_int), 'r', 'LineWidth', 1);
-set(gca,'xtickMode', 'auto', 'ytick', [])
-ylim([0, 150]);
-xlim([0 td])
-leg = legend('V LV', 'V LA');
-leg.ItemTokenSize = [10, 2];
-xlabel('t (s)');
-% pos = get(s1, 'Position');
-% set(s1, 'Position', [0.05, 0.5, 0.9,0.4])
-% set(s2, 'Position', [0.05, 0.05, 0.9,0.4])
-
-% C
-s_c = subplot(2, 2, 3);cla;hold on;
-%%
-% clf;hold on;
-title('C: Mitral flow profile')
-
-
-fill_pivot = find(i_int > 0, 1) + 65;
-i_int_f1 = i_int(1:fill_pivot);
-i_int_f2 = i_int;
-i_int_f2(1:fill_pivot) = 0;
-stop_pivot = fill_pivot + find(qmv(i_int_f2(fill_pivot+1:end)) < 0, 1);
-i_int_f2(stop_pivot:end) = 0;
-
-% fill([t(i_int_f1),fliplr(t(i_int_f1)),[t(fill_pivot); 0]], [qmv(i_int_f1), fliplr(qmv(i_int_f1));[0; 0]], 'b');
-fill([t(i_int_f1); t(fill_pivot)], [qmv(i_int_f1); 0], [0.6 0.9 1]);
-fill([t(fill_pivot);t(i_int_f2)], [0;qmv(i_int_f2)], [1 0.8 0.8]);
-
-plot(t(i_int), qmv(i_int), 'Color', [0 0.4 1], 'LineWidth', 1);
-% plot(t(i_int_f2), qmv(i_int_f2));
-% fill([t(i_int), ])
-xlabel('t (s)');
-ylabel('Flow [L/min]');
-
-%%
-% D
-s_d = subplot(2, 2, 4);cla;hold on;
-title('D: Ventricular PV loop')
-% plot(vra(i_int), pra(i_int));
-% plot(vla(i_int), pla(i_int));
-plot(vrv(i_int), prv(i_int), 'r', 'LineWidth', 1);
-plot(vlv(i_int), plv(i_int), 'b', 'LineWidth', 1);
-xlabel('Volume (ml)');
+plot(B1_vrv(i_int_bl), B1_prv(i_int_bl), 'LineWidth', 1, 'Color', color_b);
+plot(B1_vlv(i_int_bl), B1_plv(i_int_bl), 'LineWidth', 1, 'Color', color_r);
+ylim([0, 120]);
+xlim([40 160])
 ylabel('Pressure (mmHg)');
+xlabel('Volume (ml)')
+leg = legend('RV', 'LV', 'Location', 'NorthEast');
+leg.ItemTokenSize = [10, 2];
 
-%% align the axis
-drawnow()
-pos = get(s_a2, 'Position');
+exportgraphics(gcf,'fig_R_SUM_A3.pdf', 'ContentType','vector')
+%% B - tilt data read
+datafile = '../../Results/CVS_tiltable.mat';
+% system('"c:/Program Files/Dymola 2021x/bin/dsres2sdf.exe" ../../Results/CVS_tiltable.mat ../../Results/CVS_tiltable.sdf');
+% dl_arst = '../../Results/CVS_tiltable.sdf'
 
-hm = 0.1;
-vm = 0.05;
-set(s_a1, 'Position', [hm, 0.75 + vm/2, (1 - 3*hm)/2,0.25 - vm])
-set(s_a2, 'Position', [hm, 0.5+vm, (1 - 3*hm)/2,0.25 - vm])
-
-set(s_b1, 'Position', [0.5+hm/2, 0.75 + vm/2, (1 - 3*hm)/2,0.25 - vm])
-set(s_b2, 'Position', [0.5+hm/2, 0.5+vm, (1 - 3*hm)/2,0.25 - vm])
-
-set(s_c, 'Position', [hm, 1.5*vm, (1 - 3*hm)/2,0.5 - 3*vm])
-set(s_d, 'Position', [0.5 + hm/2, 1.5*vm, (1 - 3*hm)/2,0.5 - 3*vm])
+% time_arst = h5read(dl_arst, '/Time');
 % %%
-% tw = 17;
-% set(s_a1, 'Units', 'Centimeters', 'Position', tw*[hm, 0.75 + vm/2, (1 - 3*hm)/2,0.25 - vm])
-% set(s_a2, 'Units', 'Centimeters', 'Position', tw*[hm, 0.5+vm, (1 - 3*hm)/2,0.25 - vm])
-% 
-% set(s_b1, 'Units', 'Centimeters', 'Position', tw*[0.5+hm/2, 0.75 + vm/2, (1 - 3*hm)/2,0.25 - vm])
-% set(s_b2, 'Units', 'Centimeters', 'Position', tw*[0.5+hm/2, 0.5+vm, (1 - 3*hm)/2,0.25 - vm])
-% 
-% set(s_c, 'Units', 'Centimeters', 'Position', tw*[hm, 1.5*vm, (1 - 3*hm)/2,0.5 - 3*vm])
-% set(s_d, 'Units', 'Centimeters', 'Position', tw*[0.5 + hm/2, 1.5*vm, (1 - 3*hm)/2,0.5 - 3*vm])
-% 
-% %% Draw boxes
-% annotation(fig1,'rectangle', [0 0.5 0.5 0.5]);
-% annotation(fig1,'rectangle', [0.5 0.5 0.5 0.5]);
+dl = dymload(datafile)
+time = dymget(dl, 'Time');
+t_interval_hut = [0, 600]; % interval in seconds
+td = t_interval_bl(2) - t_interval_bl(1);
+% i_int = [find(t >= t_interval(1), 1), find(t >= t_interval(2), 1)-1];
+i_int_hut = time >= 2; % get rid of the initial zeros
+safezone = 400;
+i_hut = find(time > 120, 1);
+i_hutStop = find(time > 121.5, 1);
 
+t_hut = time - t_interval_hut(1);
+pbs = dymget(dl, 'brachial_pressure_systolic')/mmHg2SI;
+pbs(1:safezone) = pbs(safezone);
+pbd = dymget(dl, 'brachial_pressure_diastolic')/mmHg2SI;
+pbd(1:safezone) = pbd(safezone);
+pbm = dymget(dl, 'brachial_pressure_mean')/mmHg2SI;
+pbm(1:safezone) = pbm(safezone);
+pa_tibial_hut = dymget(dl, 'SystemicComponent.posterior_tibial_T4_L214.p_in')/mmHg2SI;
+pv_tibial_hut = dymget(dl, 'SystemicComponent.posterior_tibial_T4_L214.p_out')/mmHg2SI;
 
-%% save figure
-tw = 17;
-th = 17;
-set(gcf, 'Units', 'Centimeters', 'Position', [0, 0, tw, th], 'PaperUnits', 'Centimeters', 'PaperSize', [tw, th])
-%%
-exportgraphics(gcf,'fig_R_base.png','Resolution',300)
-exportgraphics(gcf,'fig_R_base.pdf', 'ContentType','vector')
-% print(gcf,'fig_R_base_300.png', '-dpng', '-r300')
-% saveas(gcf,'fig1.svg')
+hr = dymget(dl, 'HR')/bpm2SI;
+co = dymget(dl, 'CO')*60000;
+lsv = dymget(dl, 'SV')/ml2SI;
+% rsv = dymget(dl, 'heartComponent.pulmonaryValve.SV')/ml2SI;
+
+%% B1 - tilt data plot
+figure(1);clf; hold on;
+set(gcf, 'DefaultAxesFontSize', 6, 'defaultLineLineWidth',1.0);
+tw = 6.2;
+th = 4.5;
+set(gcf, 'Units', 'Centimeters', 'Position', [0, 0, tw, th])
+
+psv = dymget(dl, 'P_sv')/mmHg2SI;
+ppv = dymget(dl, 'P_pv')/mmHg2SI;
+
+pbp = fill([t_hut;flipud(t_hut)]/60, [pbs;flipud(pbd)], color_lb,'EdgeColor',[182/255, 226/255, 1]);
+pbpm = plot(t_hut(i_int_hut)/60, pbm(i_int_hut), 'LineWidth', 2, 'Color', color_b);
+ylabel('Pressure (mmHg)');
+ylim([60, 130]);
+
+yyaxis right;
+% set(gca,{'ycolor'},{'r';'r'})
+phr = plot(t_hut/60, hr, 'r', 'LineWidth', 1, 'Color', color_r);
+% pco = plot(t(i_int)/60, co(i_int), 'LineWidth', 1, 'Color', color_m);
+ylim([60, 130])
+g = gca; g.YAxis(2).Color = [0,0,0];
+ylabel('Frequency (BPM)');
+xlabel('t (min)');
+leg = legend('PA', 'PAm', 'HR*', 'Location', 'NorthEast');
+leg.ItemTokenSize = [10, 2];
+
+exportgraphics(gcf,'fig_R_SUM_B1.pdf', 'ContentType','vector')
+%% B2 - tilt peripherals
+figure(1);clf; hold on;
+set(gcf, 'DefaultAxesFontSize', 6, 'defaultLineLineWidth',1.0);
+tw = 5.8;
+th = 4.5;
+set(gcf, 'Units', 'Centimeters', 'Position', [0, 0, tw, th])
+
+int_hut = [i_hut:i_hutStop];
+plot(t_bl(i_int_bl2), pa_tibial(i_int_bl2), ':', 'Color', color_r);
+plot(t_bl(i_int_bl2), pv_tibial(i_int_bl2), ':', 'Color', color_b);
+plot(t_hut(int_hut) - time(i_hut), pa_tibial_hut(int_hut), 'Color', color_r);
+plot(t_hut(int_hut)- time(i_hut), pv_tibial_hut(int_hut), 'Color', color_b);
+ylim([0, 220])
+leg = legend('L214 Arterial (supine)', 'L214 Venous (supine)', 'L214 Arterial (HUT)', 'L214 Venous (HUT)', 'Location', 'SouthEast');
+leg.ItemTokenSize = [10, 2];
+ylabel('Pressure (mmHg)');
+xlabel('t (s)');
+exportgraphics(gcf,'fig_R_SUM_B2.pdf', 'ContentType','vector')
+
+%% C - valsalva
+datafile = '../../Results/CVS_valsalva.mat';
+dl = dymload(datafile);
+
+time = dymget(dl, 'Time');
+t_interval = [0, 60]; % interval in seconds
+td = t_interval(2) - t_interval(1);
+% i_int = [find(t >= t_interval(1), 1), find(t >= t_interval(2), 1)-1];
+i_int = time >= 2; % get rid of the initial zeros
+
+t = time - t_interval(1);
+pb = dymget(dl, 'brachial_pressure')/mmHg2SI;
+pbs = dymget(dl, 'brachial_pressure_systolic')/mmHg2SI;
+pbd = dymget(dl, 'brachial_pressure_diastolic')/mmHg2SI;
+pbm = dymget(dl, 'brachial_pressure_mean')/mmHg2SI;
+hr = dymget(dl, 'HR')/bpm2SI;
+% lsv = dymget(dl, 'SV')/ml2SI;
+% rsv = dymget(dl, 'heartComponent.pulmonaryValve.CO')./dymget(dl, 'HR')/ml2SI;
+
+% plot C
+figure(1);clf; hold on;
+set(gcf, 'DefaultAxesFontSize', 6, 'defaultLineLineWidth',1.0);
+tw = 5.8;
+th = 4.5;
+set(gcf, 'Units', 'Centimeters', 'Position', [0, 0, tw, th])
+x = 400;
+p_vm = fill([time(x:end); flipud(time(x:end))], [pbs(x:end); flipud(pbd(x:end))], color_lb,'EdgeColor',color_lb);
+% draw the simulation output
+% pbp = plot(t_bl, pb, 'b');
+pbpm = plot(t(i_int), pbm(i_int), 'LineWidth', 1, 'Color', color_b);
+phr = plot(t, hr, 'LineWidth', 1, 'Color', color_r);
+
+leg = legend('PA [mmHg]', 'PA mean (mmHg)', 'HR [BPM]', 'Location', 'NorthWest');
+leg.ItemTokenSize = [10, 10];
+ylim([50, 160])
+ylabel(' ');
+xlabel('t (s)')
+exportgraphics(gcf,'fig_R_SUM_C.pdf', 'ContentType','vector');
+
+%% D: hemorrhage
+datafile = '../../Results/Hemorrhage.mat';
+dl = dymload(datafile);
+
+time = dymget(dl, 'Time');
+t_interval = [0, time(end)]; % interval in seconds
+td = t_interval(2) - t_interval(1);
+% i_int = [find(t >= t_interval(1), 1), find(t >= t_interval(2), 1)-1];
+i_int = time >= 2; % get rid of the initial zeros
+
+t = time - t_interval(1);
+pb = dymget(dl, 'brachial_pressure')/mmHg2SI;
+pbs = dymget(dl, 'brachial_pressure_systolic')/mmHg2SI;
+pbd = dymget(dl, 'brachial_pressure_diastolic')/mmHg2SI;
+pbm = dymget(dl, 'brachial_pressure_mean')/mmHg2SI;
+hr = dymget(dl, 'HR')/bpm2SI;
+
+figure(1);clf; hold on;
+set(gcf, 'DefaultAxesFontSize', 6, 'defaultLineLineWidth',1.0);
+tw = 5.8;
+th = 4.5;
+set(gcf, 'Units', 'Centimeters', 'Position', [0, 0, tw, th])
+x = 20;
+p_m = fill([time(x:end); flipud(time(x:end))]/60, [pbs(x:end); flipud(pbd(x:end))], color_lb,'EdgeColor',color_lb);
+% draw the simulation output
+% pbp = plot(t_bl, pb, 'b');
+pbpm = plot(t(x:end)/60, pbm(x:end), 'LineWidth', 1, 'Color', color_b);
+phr = plot(t(x:end)/60, hr(x:end), 'LineWidth', 1, 'Color', color_r);
+
+leg = legend('PA [mmHg]', 'PA mean (mmHg)', 'HR [BPM]', 'Location', 'NorthWest');
+leg.ItemTokenSize = [10, 10];
+ylim([50, 160])
+ylabel(' ');
+xlabel('t (min)')
+exportgraphics(gcf,'fig_R_SUM_D.pdf', 'ContentType','vector');
+
+%% E: Exercise
+datafile = '../../Results/ExStep_base.mat';
+dl  = dymload(datafile)
+time = dymget(dl, 'Time');
+
+pb = dymget(dl, 'brachial_pressure')/mmHg2SI;
+pbs = dymget(dl, 'brachial_pressure_systolic')/mmHg2SI;
+pbd = dymget(dl, 'brachial_pressure_diastolic')/mmHg2SI;
+pbm = dymget(dl, 'brachial_pressure_mean')/mmHg2SI;
+CO = dymget(dl, 'CO')*60000;
+el = dymget(dl, 'Exercise.y_');
+
+ts = 35
+te = 36.6
+td = te - ts;
+diffs = [1; find( diff(el) > 0)];
+ef = [];
+edv = [];
+pow_lvs = [];
+pow_rvs = [];
+co_s = [];
+q_ex_s = [];
+
+pbs_s = [];
+pbd_s = [];
+pbm_s = [];
+
+for i = 1:size(diffs)
+    i_s = find(time > time(diffs(i)) + ts, 1);
+    i_e = find(time > time(diffs(i)) + te, 1);
+    i_int = [i_s:i_e];
+    co_s = [co_s, mean(CO(i_int))];
+    pbs_s = [pbs_s, mean(pbs(i_int))];
+    pbd_s = [pbd_s, mean(pbd(i_int))];
+    pbm_s = [pbm_s, mean(pbm(i_int))];
+end
+
+figure(1);clf; hold on;
+set(gcf, 'DefaultAxesFontSize', 6, 'defaultLineLineWidth',1.0);
+tw = 6.2;
+th = 4.5;
+set(gcf, 'Units', 'Centimeters', 'Position', [0, 0, tw, th])
+
+% plot([0:10:100], edv, 'b*-')
+% plot([0:10:100], pow_rvs, 'r*-')
+% plot([0:10:100], pow_lvs, 'b*-')
+% plot([0:10:100], ef*10, 'b*-')
+el_s = [0:10:100]';
+fill([el_s; flipud(el_s)], [pbs_s'; flipud(pbd_s')], color_lb,'EdgeColor',color_lb);
+plot(el_s, pbm_s, 'Color', color_b);
+ylabel('Pressure (mmHg)')
+ylim([50, 250])
+yyaxis right;
+plot([0:10:100], co_s, '--', 'Color', color_m, 'LineWidth', 1.5);
+ylim([5, 25])
+ylabel('CO (L/min)')
+xlabel('Exercise level (% of max)')
+g = gca; g.YAxis(2).Color = [0,0,0];
+leg = legend('PA', 'PA mean', 'CO*', 'Location', 'NorthWest');
+leg.ItemTokenSize = [10, 10];
+exportgraphics(gcf,'fig_R_SUM_E.pdf', 'ContentType','vector');
+
+% plot([0:10:100], q_ex_s, 'r*-')
+% title('Ejection fraction on exercise level')
